8bd1aa8aa0125269a578124ee066317cdc22bbc1
krishna sahu <106742852+atmananda1@users.noreply.github.com>
2022-06-08 18:10:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 8th june - 05:21 AM - webhooks demo2 expirement12!");
+    System.out.println("Hello World 8th june - 05:21 AM - webhooks demo2 Born to succeed!");
     }
 }
